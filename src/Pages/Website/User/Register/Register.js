11ea39c791bb095122fdc83125aa139d9f1cd4ba
Rob <Robertpaullawson@gmail.com>
2020-10-04 16:58:39 +0100

    Register

--- src/Pages/Website/User/Register/Register.js
+++ src/Pages/Website/User/Register/Register.js
@@ -1,7 +1,9 @@
 import React, { useState, Fragment } from 'react';
+import axios from 'axios';
 
 /* React Hook Form */
 import { useForm } from 'react-hook-form';
+
 
 // Material-UI
 import { makeStyles } from '@material-ui/core/styles';
@@ -10,8 +12,10 @@ import {
 	FormControl,
 	Typography,
 	TextField,
-    Avatar,
+	Avatar,
+	Container,
 } from '@material-ui/core';
+
 
 // Icons
 import LockIcon from '@material-ui/icons/Lock';
@@ -23,20 +27,20 @@ import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 // React Router + utils
 import { ForgotRoute } from '../../../../Routing'
 import { Link } from 'react-router-dom';
-import { blue } from '@material-ui/core/colors';
-// import { LoginRoute } from '../../../../Routing'
+import { OverviewRoute } from '../../../../Routing'
 
 // Styles 
 const useStyles = makeStyles((theme) => ({
 	form: {
 		marginTop: '20px',
-		width: '80%',
 		display: 'grid',
+	    width: '100%',
 		direction: 'column',
 		justify: 'center',
 		alignItems: 'center',
 		margin: 'auto',
 	},
+	
 	label: {
 		padding: theme.spacing(2),
 		textAlign: 'center',
@@ -59,101 +63,132 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-const RegisterForm = ( ) => { 
+const RegisterForm = ( props) => { 
 	
-	const [newUser, SetNewUser] = useState({  
-		email: '',
-		username: '',
-		password: ''
-	
-	})
+	const { register, errors, handleSubmit, control } = useForm({
+		mode: 'onChange',
+		reValidateMode: 'onChange',
+		defaultValues: {
+		  email: '',
+		  username: '',
+		  password: '',
+		},
+	  });
 
-	const user = {
-		email: newUser.email,
-		username: newUser.username,
-		password: newUser.password
-	}
-	const onSubmit = async (values) => {
-		console.log(values);
-		const {data} = await axios.post('/auth', { ...values});
-		console.log(data);
-	  };
-	
 
+	const onSubmit = async (email, password, username) => {
+	    try {
+			const res = await axios.post('/users/register', { user: email, password, username });
+			if (res) {
+			  setTimeout(() => {
+				props.history.push(OverviewRoute);
+			  }, 2000);
+			}
+		  } catch (error) {
+			console.log(error);
+		  }
+		};
+		
+		
 	
 	const classes = useStyles();
 	
 	return ( 
-	<Fragment  >
-		
+	<Container maxWidth='xs' component='main' >
+
+	
+	
 		<Avatar className={classes.Avater}>
 								<AssignmentIndIcon  />
 						</Avatar>
 
 			<h1 className={classes.h1}>Please Register Below</h1>	
 				 {/* form  */}
-				<form className={classes.form} >
+			
+				<form className={classes.form} 
+				   onSubmit={handleSubmit(onSubmit)} >
 				{/* Email */}
 						<TextField 
-							error
+							id='email'
+							label='Email Address'
+							type='email'
+							name='email'
 							variant='outlined' 
 							margin='normal'
-							label='Email Address'
-							value={newUser.email}
-							onChange={(e) => SetNewUser(e.target.value)} 
+							fullWidth
 							
+							inputRef={register({
+								required: 'Required',
+								pattern: {
+									value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+									message: 'Invalid email address',
+								},
+								})}
 							InputProps={{
 							startAdornment: (
 								<InputAdornment position="start">
 								<EmailIcon color='secondary'/>
 								</InputAdornment>
 								),
-        					}}/>
+        					}}
+							error={!!errors.email}
+							/>
+							  {errors.email && errors.email.message}
 				{/*username  */}
 				<TextField 
+							fullWidth	
 							margin='normal'
 							variant='outlined' 
 							label='Username'
 							name='username' 
-							value={newUser.username}
-							onChange={(e) => SetNewUser(e.target.value)} 
+							required={true}
 							InputProps={{
 							startAdornment: (
 								<InputAdornment position="start">
 								<AccountCircleIcon color='secondary' />
 								</InputAdornment>
 								),
-        					}}/>
+        					}}
+							error={!!errors.username}
+							/>
+							{errors.username && errors.username.message}
+
 				{/* Password */}
 				<TextField
-							error
+							
 							margin='normal'
 							variant='outlined' 
 							label='Password' 
 							name="password"
-							value={newUser.password}
-							onChange={(e) => SetNewUser(e.target.value)} 
-							
+							inputRef={register({
+								required: 'Required',
+								pattern: {
+								value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+								message: 'Please include at least 1 character and 1 number',
+								},
+								})}
 							InputProps={{
 							startAdornment: (
 								<InputAdornment position="start">
 								<LockIcon color='secondary' />
 								</InputAdornment>
           						),
-    			   		 }}/>
+    			   		 }}
+							error={!!errors.password}
+							/>
+							      {errors.password && errors.password.message}
 					<Button	 	
-						// component={Link} to={}
-						onClick={onSubmit}
-						
+						component={Link} to={OverviewRoute}
+						margin='normal'
+						fullWidth
 						variant='contained'
-						color='secondary'
-						className={classes.Avater}
+						color='primary'
+						disabled={!!errors.email || !!errors.password}
+
 					>	Register</Button>
 		</form>
 	
-					
 		<div>
-		
 			 <Typography
 				variant='body2'
 				color='initial'
@@ -163,11 +198,9 @@ const RegisterForm = ( ) => {
 				Forgot Password
 			</Typography> 
 			</div>
-			
-	
-	</Fragment>
+	</Container>
 );	
-		}
+						}
 
 const Register = (props) => {
 	    
--- src/Routing.js
+++ src/Routing.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 import React, { lazy } from 'react';
-=======
-import React from 'react';
-import { Route, withRouter } from 'react-router-dom';
-import Home from './Pages/Website/Home/Home';
-import Features from './Pages/Website/Features/Features';
-import Docs from './Pages/Website/Docs/Docs';
-import Contact from './Pages/Website/Contact/Contact';
-import Register from './Pages/Website/User/Register/Register';
-import Login from './Pages/Website/User/Login/Login';
-import Forgot from './Pages/Website/User/Forgot/Forgot';
-import Reset from './Pages/Website/User/Reset/Reset';
-import Overview from './Pages/App/Overview/Overview';
->>>>>>> c89697a3c4a3eafc4857d450d695296b5299a5e6
 
 import { Route, withRouter, Switch } from 'react-router-dom';
 
