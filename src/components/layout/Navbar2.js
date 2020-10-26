77c0f331e6dee25343d4939e896b2d56abd8df8b
Gabor Csecsetka <csecsi85@gmail.com>
2020-10-26 18:14:12 +0000

    Fixed top navigation user side alignment issue and commented code

--- src/components/layout/Navbar2.js
+++ src/components/layout/Navbar2.js
@@ -1,8 +1,16 @@
 import React, { Fragment, useContext } from 'react';
 
-// React Router + utils
-import { HomeRoute, FeaturesRoute, DocsRoute, ContactRoute, LoginRoute, RegisterRoute, ProfileRoute } from '../../Routing';
+// React Router + Routes
 import { Link, withRouter } from 'react-router-dom';
+import {
+	HomeRoute,
+	FeaturesRoute,
+	DocsRoute,
+	ContactRoute,
+	LoginRoute,
+	RegisterRoute,
+	ProfileRoute,
+} from '../../Routing';
 
 /* User Context */
 import CurrentUserContext from '../../contexts/current-user/current-user.context';
@@ -17,12 +25,14 @@ import {
 	IconButton,
 	MenuItem,
 	Menu,
+	Button,
+	Dialog,
+	DialogTitle,
+	Slide,
+	DialogActions,
 } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import DialogActions from '@material-ui/core/DialogActions';
+
+// Material-UI Icons
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
@@ -42,71 +52,84 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-function Logout(props) {
-
+// Logout Modal
+const Logout = withRouter((props) => {
 	/* get User Context */
 	const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+	// Handle popup state
+	const [open, setOpen] = React.useState(false);
 
+	// Handling logout function
+	const handleLogout = () => {
+		// Clear local storage
+		window.localStorage.clear();
+		// Empty userContext
+		setCurrentUser(null);
+		// Close popup
+		handlePopupClose();
+		// Closes user navigation menu
+		props.handleClose();
+		// Redirect to Home
+		props.history.push(HomeRoute);
+	};
 
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handlePopupClose = () => {
+		setOpen(false);
+	};
 
-const handleLogout = () => {
- window.localStorage.clear();
- setCurrentUser(null);
- handlePopupClose();
- props.history.push(HomeRoute);
-}
-const [open, setOpen] = React.useState(false);
+	return (
+		<div>
+			{/* Menuitem in profile menu */}
+			<MenuItem
+				onClick={handleClickOpen}
+				style={{ textAlign: 'left', paddingLeft: 0 }}
+			>
+				Logout
+			</MenuItem>
+			{/* The popup element */}
+			<Dialog
+				TransitionComponent={Transition}
+				open={open}
+				onClose={handlePopupClose}
+				aria-labelledby='alert-dialog-slide-title'
+				aria-describedby='alert-dialog-slide-description'
+			>
+				<DialogTitle id='alert-dialog-slide-title'>
+					{' Are you sure you want to leave?'}
+				</DialogTitle>
+				<DialogActions>
+					<Button onClick={handlePopupClose} color='primary'>
+						Stay
+					</Button>
+					<Button onClick={handleLogout} color='primary' autoFocus>
+						Logout
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	);
+});
 
-const handleClickOpen = () => {
-setOpen(true);
-};
-const handlePopupClose = () => {
-setOpen(false);
-};
-const classes = useStyles();
-
-return (
- <div> 
-   <Button  onClick={handleClickOpen}> Logout   </Button>
-   <Dialog
-   TransitionComponent={Transition}
-   
-   open={open}
-   onClose={handlePopupClose}
-   aria-labelledby="alert-dialog-slide-title"
-   aria-describedby="alert-dialog-slide-description"
-	 >
-   <DialogTitle id="alert-dialog-slide-title">{" Are you sure you want to leave?"}</DialogTitle>
-   <DialogActions>
-	 <Button onClick={handlePopupClose} color="primary">
-	   Stay
-	 </Button>
-	 <Button onClick={handleLogout} color="primary" autoFocus>
-	   Logout
-	 </Button>
-   </DialogActions>
- </Dialog>
-
-</div>
-);
-}
-
+// Modal move up transition
 const Transition = React.forwardRef(function Transition(props, ref) {
-	return <Slide direction="up" ref={ref} {...props} />;
-  });
+	return <Slide direction='up' ref={ref} {...props} />;
+});
 
-
+// Top app bar component
 function MenuAppBar(props) {
-
 	const classes = useStyles();
+	// These keep track of opened/closed states of dropdowns
 	const [anchorEl, setAnchorEl] = React.useState(null);
 	const [anchorEl2, setAnchorEl2] = React.useState(null);
 
+	// Close both dropdowns
 	const handleClose = () => {
 		setAnchorEl(null);
 		setAnchorEl2(null);
 	};
-
 
 	/* get User Context */
 	const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
@@ -115,6 +138,7 @@ function MenuAppBar(props) {
 		<div className={classes.root}>
 			<AppBar position='static'>
 				<Toolbar>
+					{/* Left side icon in app bar */}
 					<IconButton
 						edge='start'
 						className={classes.menuButton}
@@ -127,6 +151,7 @@ function MenuAppBar(props) {
 					>
 						<MenuIcon />
 					</IconButton>
+					{/* Left side drop down menu */}
 					<Menu
 						id='simple-menu'
 						name='el1'
@@ -148,13 +173,16 @@ function MenuAppBar(props) {
 							Contact
 						</MenuItem>
 					</Menu>
-
+					{/* Will be the logo here one day :D */}
 					<Typography variant='h6' className={classes.title}>
 						Trip Mate
 					</Typography>
 					<div>
+						{/* Right side icon on app bar */}
+						{/* If logged in */}
 						{currentUser ? (
 							<Fragment>
+								{/* The avatar icon component wrapper */}
 								<IconButton
 									aria-label='account of current user'
 									aria-controls='simple-menu'
@@ -162,12 +190,14 @@ function MenuAppBar(props) {
 									onClick={(e) => setAnchorEl2(e.currentTarget)}
 									color='inherit'
 								>
+									{/* The avatar itself */}
 									<Avatar
 										alt={currentUser.name}
 										src={`https:${currentUser.avatar}`}
 										className={classes.small}
 									/>
 								</IconButton>
+								{/* Right side dropdown menu and menu items */}
 								<Menu
 									id='simple-menu'
 									anchorEl={anchorEl2}
@@ -190,20 +220,15 @@ function MenuAppBar(props) {
 									>
 										Profile
 									</MenuItem>
-									{/* <MenuItem
-										onClick={handleClose}
-										component={Link}
-										to={RegisterRoute}
-									>
-										Settings
-									</MenuItem> */}
 									<MenuItem>
-										<Logout />
+										<Logout handleClose={handleClose} />
 									</MenuItem>
 								</Menu>
 							</Fragment>
 						) : (
+							// If logged out
 							<Fragment>
+								{/* Right side icon wrapper */}
 								<IconButton
 									aria-label='account of current user'
 									aria-controls='simple-menu'
@@ -211,8 +236,10 @@ function MenuAppBar(props) {
 									onClick={(e) => setAnchorEl2(e.currentTarget)}
 									color='inherit'
 								>
+									{/* The icon itself */}
 									<AccountCircle />
 								</IconButton>
+								{/* Right side drop down menu and menu items */}
 								<Menu
 									id='simple-menu'
 									anchorEl={anchorEl2}
@@ -252,6 +279,4 @@ function MenuAppBar(props) {
 	);
 }
 
-
-
-export default withRouter(MenuAppBar)
+export default withRouter(MenuAppBar);
