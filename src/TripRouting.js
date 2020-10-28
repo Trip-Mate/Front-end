ee8fa6d14dd229b2eed098c4cfcac897195dbff7
Gabor Csecsetka <csecsi85@gmail.com>
2020-10-28 00:30:41 +0000

    Defined day route and added boilerplate component

--- src/TripRouting.js
+++ src/TripRouting.js
@@ -5,8 +5,11 @@ import { Route, withRouter, Switch } from 'react-router-dom';
 import SingleTrip from './Pages/App/Trip/SingleTrip';
 
 const TripPlan = lazy(() => import('../src/Pages/App/Trip/trip-plan'));
+const SingleDay = lazy(() => import('./Pages/App/Day/Day'));
+
 
 export const SingleTripRoute = `/trips/:id`;
+export const SingleDayRoute = `/days/:dayID`
 
 function TripRouting () {
 
@@ -14,6 +17,7 @@ function TripRouting () {
         <Switch>
             <Route path={`${SingleTripRoute}/trip-plan`} exact component={TripPlan} />
             <Route path={SingleTripRoute} exact component={SingleTrip} />
+            <Route path={`${SingleTripRoute}${SingleDayRoute}`} exact component={SingleDay} />
         </Switch>
     )
 };
