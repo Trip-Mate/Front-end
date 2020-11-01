3526146c221324fef2572828c22ecfcad472f51c
Gabor Csecsetka <csecsi85@gmail.com>
2020-11-01 01:00:03 +0000

    Added basic snapshot to Overview

--- src/Pages/App/Overview/Overview.js
+++ src/Pages/App/Overview/Overview.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { HomeRoute, NewTripRoute, ContactRoute, MyTripsRoute } from '../../../Routing'
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import {
+	HomeRoute,
+	NewTripRoute,
+	ContactRoute,
+	MyTripsRoute,
+} from '../../../Routing';
+import { NavLink } from 'react-router-dom';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Container,
-  Typography,
-}  from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
-// Icons 
+// Icons
 import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 import PersonIcon from '@material-ui/icons/Person';
 import LanguageIcon from '@material-ui/icons/Language';
@@ -18,159 +20,149 @@ import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import ShareIcon from '@material-ui/icons/Share';
 
 const useStyles = makeStyles((theme) => ({
-  Container: {
-    marginTop: theme.spacing(8),
-    margin: 'auto',
-    display: 'grid',
-    gridAutoFlow: 'column',
-    
-  },
-  mainDiv: {
-     display: 'grid',
-  },
+	Container: {
+		marginTop: theme.spacing(8),
+		margin: 'auto',
+		display: 'grid',
+		gridAutoFlow: 'column',
+	},
+	mainDiv: {
+		display: 'grid',
+	},
 
-  icons: {
-    gridAutoFlow: 'column',
-    margin: 'auto ',
-    border: '3px solid 	#3b5998',
-    borderRadius: '50%' ,
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    padding:'13px',
-    fontSize: 'large',
-    lineHeight: '0',
-  },
-  title:{
-    color: '	#3b5998',
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize: '18px',
-    fontWeight: 'bolder',
-    letterSpacing: '1px',
-    BoxShadow: '5px',
-    
-  },
-  image: {
-    position: 'absolute',
-    zIndex: '-1',
-    top: '56px',
-    backgroundSize: 'cover',
-    width: '100%',
-    height: 'calc(100% - 56px)',
-    // filter: 'grayscale(100%)',
-    opacity: '0.4',
-    
-  },
-  link: {
-    color: '	#3b5998 ',
-  },
-  hover: {
-    '&:hover': {
-      animation: `$spin 3s`,
-   }
-  },
-    '@keyframes spin': {
-      '25%': {
-        transform:' rotateY(-60deg)',
-        color: 'blue',
-        animationTimingFunction: 'ease',
-      },
-      
-      '100%': {
-        color: 'blue',
-        transform: 'rotateY(360deg)',
-      },
-    },
-  
-  
-    // "&:hover": {
-    //   backgroundColor: 'rgb(7, 177, 77, 0.42)'
-    // }
-  
+	icons: {
+		gridAutoFlow: 'column',
+		margin: 'auto ',
+		border: '3px solid 	#3b5998',
+		borderRadius: '50%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		textAlign: 'center',
+		padding: '13px',
+		fontSize: 'large',
+		lineHeight: '0',
+	},
+	title: {
+		color: '	#3b5998',
+		alignItems: 'center',
+		textAlign: 'center',
+		fontSize: '18px',
+		fontWeight: 'bolder',
+		letterSpacing: '1px',
+		BoxShadow: '5px',
+	},
+	image: {
+		position: 'absolute',
+		zIndex: '-1',
+		top: '56px',
+		backgroundSize: 'cover',
+		width: '100%',
+		height: 'calc(100% - 56px)',
+		// filter: 'grayscale(100%)',
+		opacity: '0.4',
+	},
+	link: {
+		color: '	#3b5998 ',
+	},
+	hover: {
+		'&:hover': {
+			animation: `$spin 3s`,
+		},
+	},
+	'@keyframes spin': {
+		'25%': {
+			transform: ' rotateY(-60deg)',
+			color: 'blue',
+			animationTimingFunction: 'ease',
+		},
 
+		'100%': {
+			color: 'blue',
+			transform: 'rotateY(360deg)',
+		},
+	},
+
+	// "&:hover": {
+	//   backgroundColor: 'rgb(7, 177, 77, 0.42)'
+	// }
 }));
 
-function Overview(props) {
+function Overview() {
+	const classes = useStyles();
 
-
-  
-
-  const classes = useStyles();
-
-  return (
-    <div>
-    <img className={classes.image} 
-    src='https://images.unsplash.com/photo-1527422265102-22027ee90fcd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80' 
-    alt="background" />
-    <Container className={classes.Container} >
-          <div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={MyTripsRoute} > 
-                  <FlightTakeoffIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>My Trips</Typography>
-            </div>
-          </div>
-          <div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={HomeRoute} > 
-                  <PersonIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>Home</Typography>
-            </div>
-          </div>
-          <div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={NewTripRoute} > 
-                  <LanguageIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>Plan Trip</Typography>
-            </div>
-          </div>
-    </Container>
-
-    <Container className={classes.Container} >
-    <div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={HomeRoute} > 
-                  <AccessTimeIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>Random?</Typography>
-            </div>
-          </div>
-          <div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={HomeRoute} > 
-                  <AssignmentIndIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>Contact Us</Typography>
-            </div>
-          </div><div  className={classes.mainDiv}>
-            <div  className={classes.icons}  >
-                <NavLink className={classes.link}   to={ContactRoute} > 
-                  <ShareIcon   fontSize='large' className={classes.hover} />
-                </NavLink>
-              </div>
-              <div>
-                <Typography className={classes.title}>Share Trips</Typography>
-            </div>
-          </div>
-    </Container>
-
-
-  </div>
-  )
+	return (
+		<div>
+			<img
+				className={classes.image}
+				src='https://images.unsplash.com/photo-1527422265102-22027ee90fcd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+				alt='background'
+			/>
+			<Container className={classes.Container}>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={MyTripsRoute}>
+							<FlightTakeoffIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>My Trips</Typography>
+					</div>
+				</div>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={HomeRoute}>
+							<PersonIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>Home</Typography>
+					</div>
+				</div>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={NewTripRoute}>
+							<LanguageIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>Plan Trip</Typography>
+					</div>
+				</div>
+			</Container>
+			<Container className={classes.Container}>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={HomeRoute}>
+							<AccessTimeIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>Random?</Typography>
+					</div>
+				</div>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={HomeRoute}>
+							<AssignmentIndIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>Contact Us</Typography>
+					</div>
+				</div>
+				<div className={classes.mainDiv}>
+					<div className={classes.icons}>
+						<NavLink className={classes.link} to={ContactRoute}>
+							<ShareIcon fontSize='large' className={classes.hover} />
+						</NavLink>
+					</div>
+					<div>
+						<Typography className={classes.title}>Share Trips</Typography>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
 }
 
-export default Overview
+export default Overview;
