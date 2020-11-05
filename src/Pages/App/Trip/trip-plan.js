b512e08b1811bb520b36a5d029c59f5addefb09b
Gabor Csecsetka <csecsi85@gmail.com>
2020-11-05 01:29:54 +0000

    Fix error message on trip-plan route after page reload

--- src/Pages/App/Trip/trip-plan.js
+++ src/Pages/App/Trip/trip-plan.js
@@ -1,73 +1,87 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import axios from 'axios';
 
 import SingleTripContext from '../../../contexts/single-trip/single-trip.context';
+import Spinner from '../../../components/Spinner/Spinner';
 
 // DayComponent needs to be implemented
-import Day from '../../../components/Day/Day.component';
+// import Day from '../../../components/Day/Day.component';
 
 const TripPlan = ({ match }) => {
 
 	const { singleTrip: { days }, setSingleTrip } = useContext(SingleTripContext);
+	const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
-		(async () => {
-			try {
+			(async () => {
+				try {
+					/* getting user token */
+					const user = JSON.parse(localStorage.getItem('user'));
+					const token = user.token;
+					const tripID = match.params.id;
 
-				/* getting user token */
-				const user = JSON.parse(localStorage.getItem('user'));
-				const token = user.token;
-				const tripID = match.params.id;
+					/* getting authorized response */
+					const res = await axios.get(`/trips/${tripID}/trip-plan`, {
+						headers: {
+							'x-auth-token': token,
+						},
+						params: {
+							id: tripID,
+						},
+					});
 
-				/* getting authorized response */
-				const res = await axios.get(`/trips/${tripID}/trip-plan`, {
-					headers: {
-						'x-auth-token': token,
-					},
-					params: {
-						'id': tripID,
-					}
-				});
+					/* getting trip days */
+					const daysIDs = await res.data;
+					console.log('Days Data', daysIDs);
 
-				/* getting trip days */
-                const daysIDs = await res.data;
-				console.log('Days Data', daysIDs);
-				
-				/* passing days Ids to Use State Days*/
-				setSingleTrip(daysIDs);
-
-			} catch (error) {
-				console.log('Error', error.message);
-			}
-		})();
-	}, []);
+					/* passing days Ids to Use State Days*/
+					setSingleTrip(daysIDs);
+					setIsLoaded(true)
+				} catch (error) {
+					console.log('Error', error.message);
+				}
+			})();
+		}, [match.params.id, setSingleTrip]);
 
 	console.log('Days Context', days);
     
-    return(
-
-		/* Demo Container */
-        <section
-		style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', contentVisibility: 'auto', marginBottom: '60px', }}
-		>
-			{
-				!days?.length > 0 ? (
-					<h2>There are no days in here 😖</h2>
+    return (
+			/* Demo Container */
+			<section
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-evenly',
+					alignItems: 'center',
+					contentVisibility: 'auto',
+					marginBottom: '60px',
+				}}
+			>
+				{isLoaded ? !days?.length > 0 ? (
+					<h2>
+						There are no days in here{' '}
+						<span role='img' aria-label='Sad face'>
+							😖
+						</span>
+					</h2>
 				) : (
 					days.map((day, idx) => (
-						<div 
-						key={day._id}
-						style={{ height: '20vh', width: '50vw', margin: '5%', border: '1px solid black', }}
+						<div
+							key={day._id}
+							style={{
+								height: '20vh',
+								width: '50vw',
+								margin: '5%',
+								border: '1px solid black',
+							}}
 						>
-							Day: {idx+1}
+							Day: {idx + 1}
 						</div>
 					))
-				)
-				 
-			}
-		</section>
-    )
+				) : <Spinner />}
+			</section>
+		);
 };
 
 export default TripPlan;
