04b28d55f5afb299c9109fb2d4b17dad8305de3e
Gabor Csecsetka <csecsi85@gmail.com>
2020-11-13 03:47:15 +0000

    Slightly improved design for notes list

--- src/Pages/App/Trip/QuickNotes.js
+++ src/Pages/App/Trip/QuickNotes.js
@@ -4,7 +4,52 @@ import axios from 'axios';
 
 import Spinner from '../../../components/Spinner/Spinner';
 
+import {
+	Container,
+	Grid,
+	makeStyles,
+	Paper,
+	Fab,
+	Typography,
+	IconButton
+} from '@material-ui/core';
+
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		marginBottom: '65px',
+	},
+	paper: {
+		marginTop: theme.spacing(2),
+		padding: theme.spacing(2),
+	},
+	headerContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+	},
+	header: {
+		fontSize: '1.5rem',
+	},
+	description: {
+		marginTop: theme.spacing(2),
+	},
+	iconContainer: {
+		marginTop: theme.spacing(2),
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	fab: {
+		position: 'fixed',
+		top: theme.spacing(10),
+		right: theme.spacing(2),
+	},
+}));
+
 const QuickNotes = ({ match }) => {
+	const classes = useStyles();
 	const [notes, setNotes] = useState(null)
 	const [isLoaded, setIsLoaded] = useState(false)
 
@@ -41,39 +86,41 @@ const QuickNotes = ({ match }) => {
 }, [match.params.id, setNotes]);
 
 	return (
-		<section
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'space-evenly',
-				alignItems: 'center',
-				contentVisibility: 'auto',
-				marginBottom: '60px',
-			}}
-		>
+		<Container className={classes.container}>
+			<Fab color='secondary' aria-label='add' className={classes.fab}>
+				<AddIcon />
+			</Fab>
 			{isLoaded ? (
 				!notes?.length > 0 ? (
 					<h2>There are no notes here!</h2>
 				) : (
 					notes.map((note, idx) => (
-						<div
-							key={note._id}
-							style={{
-								height: '20vh',
-								width: '50vw',
-								margin: '5%',
-								border: '1px solid black',
-							}}
-						>
-							<h1>{ note.title }</h1>
-							<p>{ note.note }</p>
-						</div>
+						<Grid item xs={12} sm={12} key={note._id}>
+							<Paper className={classes.paper}>
+								<div className={classes.headerContainer}>
+									<div>
+										<div className={classes.header}>{note.title}</div>
+									</div>
+								</div>
+								<Typography className={classes.description}>
+									{note.note}
+								</Typography>
+								<div className={classes.iconContainer}>
+									<IconButton aria-label='edit' color='primary'>
+										<EditIcon fontSize='large' />
+									</IconButton>
+									<IconButton aria-label='delete' color='secondary'>
+										<DeleteIcon fontSize='large' />
+									</IconButton>
+								</div>
+							</Paper>
+						</Grid>
 					))
 				)
 			) : (
 				<Spinner />
 			)}
-		</section>
+		</Container>
 	);
 }
 
